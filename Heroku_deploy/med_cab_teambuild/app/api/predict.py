4e2046e7c007b448b16efd00403e71384da8ea9f
CSLSDS <lambdadatasciencelearning@gmail.com>
2020-09-22 22:33:05 -0400

    +NN_model; update predict.py

--- Heroku_deploy/med_cab_teambuild/app/api/predict.py
+++ Heroku_deploy/med_cab_teambuild/app/api/predict.py
@@ -1,26 +1,129 @@
-from typing import Dict  # helps enforce typing
+from typing import Dict, Optional  # helps enforce typing
 import logging
 import random
-
+import numpy as np
 from fastapi import APIRouter
 import joblib
 import pandas as pd
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field, validator, Json
 
 
 log = logging.getLogger(__name__)
 router = APIRouter()
 
-model = joblib.load("app/api/model.joblib")
+model = joblib.load("app/api/nn_model.joblib")
 
 print("Serialized Model Loaded")
 
+# cats = ['hybrid', 'sativa', 'indica', 'Aroused', 'Creative', 'Euphoric',
+#         'Energetic', 'Euphoric', 'Focused', 'Giggly', 'Happy', 'Hungry',
+#         'Relaxed', 'Sleepy', 'Talkative', 'Tingly', 'Uplifted', 'anxiety',
+#         'depression', 'pain', 'fatigue', 'insomnia', 'brain fog',
+#         'loss of appetite', 'nausea', 'low libido']
 
-# create a class for OOP reasons.  I think we are better off hiding
-# implementation instead of exposing the direct Dict structure
-# also we will now get back more meaningful errors, 
-# because fastapi is going to parse the new object for us,
-# basically ensure a valid state for our object. 
+cats = ["body",
+        "potent",
+        "stress",
+        "relaxing",
+        "cerebral",
+        "mind",
+        "physical",
+        "uplifting",
+        "relaxation",
+        "day",
+        "cbd",
+        "euphoria",
+        "anxiety",
+        "relief",
+        "mood",
+        "appetite",
+        "mental",
+        "depression",
+        "energy",
+        "balanced",
+        "nausea",
+        "creative",
+        "insomnia",
+        "alien",
+        "good",
+        "help",
+        "stimulating",
+        "pain",
+        "fatigue",
+        "brain fog",
+        "loss of appetite",
+        "low libido",
+        "hybrid",
+        "sativa",
+        "indica",
+        "Focused",
+        "Happy",
+        "Aroused",
+        "Uplifted",
+        "Creative",
+        "Hungry",
+        "Sleepy",
+        "Giggly",
+        "Relaxed",
+        "Tingly",
+        "Energetic",
+        "Euphoric",
+        "Talkative",
+        "Grapefruit",
+        "Pear",
+        "Tree",
+        "Tobacco",
+        "Apple",
+        "Herbal",
+        "Citrus",
+        "Sage",
+        "Butter",
+        "Bluberry",
+        "Fruity",
+        "Tree Fruit",
+        "Rose",
+        "Chestnut",
+        "Skunk",
+        "Pepper",
+        "Fruit",
+        "Apricot",
+        "Mango",
+        "Tea",
+        "Vanilla",
+        "Berry",
+        "Strawberry",
+        "Menthol",
+        "Blue",
+        "Honey",
+        "Blueberry",
+        "Minty",
+        "Pine",
+        "Lavender",
+        "Flowery",
+        "Orange",
+        "Nutty",
+        "Grapes",
+        "Woody",
+        "Tropical",
+        "Peach",
+        "Grape",
+        "Diesel",
+        "Spicy",
+        "Mint",
+        "Sweet",
+        "Coffee",
+        "Chemical",
+        "Cheese",
+        "Tar",
+        "Ammonia",
+        "Bubblegum",
+        "Pineapple",
+        "Lemon",
+        "Plum",
+        "Earthy",
+        "Violet",
+        "Pungent",
+        "Lime"]
 
 class UserInputData(BaseModel):
     """Create a class for OOP reasons.  I think we are better off hiding
@@ -28,28 +131,32 @@ class UserInputData(BaseModel):
     also we will now get back more meaningful errors, 
     because fastapi is going to parse the new object for us,
     basically ensure a valid state for our object. """
-
-    ailment: str
-    primary_impact: str
-    undesired_impact: str
+    include: Optional[Dict[str, str]]
+    exclude: Optional[Dict[str, str]]
 
     def to_df(self):
-        return pd.DataFrame([dict(self)])
+        '''somehow force shape, fillna'''
+        df = pd.DataFrame(columns=cats)
+        df.loc[0] = [0.5]*len(cats) # number of training dimensions; 0.5 is null
+        for key, value in self.include.items(): # in 'include'
+            df[key] = int(bool(value)) # converts T/F to ints 1/0
+        return df
 
 
 @router.post("/predict")
 def predict_strain(user: UserInputData):
     """Predict the ideal strain based on user input"""
-    df = user.to_df()
-    # this is where the model goes
-    # ideal_strain[0] = model.predict(df)
-    return df.shape
+    X_new = user.to_df()
+    log.info(X_new)
+    neighbors = model.kneighbors(user.to_df())[1][0] # vid @ 56:02
+    return {
+        "IDs":[int(id_) for id_ in neighbors]
+    }
 
-
-@router.get('/random')  # What is this route going to be?
-def random_penguin(): 
-    """Return a random penguin species"""
-    return random.choice(["Adelie", "ChinStrap", "Gentoo"])
+# @router.get('/random')  # What is this route going to be?
+# def random_penguin(): 
+#     """Return a random penguin species"""
+#     return random.choice(["Adelie", "ChinStrap", "Gentoo"])
 
 
 # class Item(BaseModel):
